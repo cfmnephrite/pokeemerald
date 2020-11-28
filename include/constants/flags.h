36568e0ea90ada6718e4f4b161da178607de740e
Nephrite <thechurchofcage@gmail.com>
2020-11-28 02:26:59 +0000

    Guided route to start the game
    As soon as you leave the house, it's dark and you can only go towards your neighbour's house

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -794,10 +794,10 @@
 #define FLAG_HIDE_SLATEPORT_CITY_SCOTT                              0x2ED
 #define FLAG_HIDE_ROUTE_101_ZIGZAGOON                               0x2EE
 #define FLAG_HIDE_VICTORY_ROAD_EXIT_WALLY                           0x2EF
-#define FLAG_HIDE_LITTLEROOT_TOWN_MOM_OUTSIDE                       0x2F0
+#define FLAG_HIDE_LITTLEROOT_ALL_NPCS                               0x2F0
 #define FLAG_HIDE_MOSSDEEP_CITY_SPACE_CENTER_1F_STEVEN              0x2F1
-#define FLAG_UNUSED_2F2          0x2F2
-#define FLAG_UNUSED_2F3          0x2F3
+#define FLAG_UNUSED_2F2                                             0x2F2
+#define FLAG_UNUSED_2F3                                             0x2F3
 #define FLAG_HIDE_MOSSDEEP_CITY_SPACE_CENTER_1F_TEAM_MAGMA          0x2F4
 #define FLAG_HIDE_LITTLE_ROOT_TOWN_PLAYERS_BEDROOM_MOM              0x2F5
 #define FLAG_HIDE_LITTLEROOT_TOWN_BRENDANS_HOUSE_MOM                0x2F6
